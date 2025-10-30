bb03def0a33f1ebd76cd032a2897a31ad84d4326
Khalifa Alkharfy <alkharfy1@gmail.com>
2025-10-30 20:39:17 +0000

    Try fixing this error: `Runtime Error: SyntaxError: Expected property na

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -7,7 +7,13 @@ import { useDoc } from './firestore/use-doc';
 import { FirebaseClientProvider } from './client-provider';
 import { useUser } from './auth/use-user';
 
-const firebaseConfig: FirebaseOptions = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG || '{}');
+const firebaseConfig: FirebaseOptions = {
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+};
 
 function initializeFirebase() {
   const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
